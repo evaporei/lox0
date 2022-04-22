4be72b178aff67a3a7d1883a6406dff5e9a6c635
Eva Pace <eba.pachi@gmail.com>
2022-04-22 20:11:36 -0300

    main: Initial structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let mut args = std::env::args();
+
+    if args.len() > 1 {
+        println!("Usage: rlox [script]");
+        std::process::exit(64);
+    } else if args.len() == 1 {
+        run_file(&args.next().unwrap());
+    } else {
+        run_prompt();
+    }
 }
+
+fn run_file(_file_name: &str) {}
+
+fn run_prompt() {}
