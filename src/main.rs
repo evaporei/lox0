a40dc7f2d913af7a15bccd67a773b87633cadab7
Eva Pace <eba.pachi@gmail.com>
2022-04-22 20:15:04 -0300

    main: Implement 'run_file' function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = std::env::args();
 
     if args.len() > 1 {
         println!("Usage: rlox [script]");
         std::process::exit(64);
     } else if args.len() == 1 {
-        run_file(&args.next().unwrap());
+        run_file(&args.next().unwrap())?;
     } else {
         run_prompt();
     }
+
+    Ok(())
 }
 
-fn run_file(_file_name: &str) {}
+fn run_file(file_path: &str) -> std::io::Result<()> {
+    run(&std::fs::read_to_string(file_path)?);
+    Ok(())
+}
 
 fn run_prompt() {}
+
+fn run(_source: &str) {}
