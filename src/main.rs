80113662dc74c4f0cec0705c93e7e05a8daa6858
Eva Pace <eba.pachi@gmail.com>
2022-04-22 20:21:09 -0300

    main: Implement 'run_prompt' function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{self, BufRead};
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = std::env::args();
 
@@ -7,17 +9,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     } else if args.len() == 1 {
         run_file(&args.next().unwrap())?;
     } else {
-        run_prompt();
+        run_prompt()?;
     }
 
     Ok(())
 }
 
-fn run_file(file_path: &str) -> std::io::Result<()> {
+fn run_file(file_path: &str) -> io::Result<()> {
     run(&std::fs::read_to_string(file_path)?);
     Ok(())
 }
 
-fn run_prompt() {}
+fn run_prompt() -> io::Result<()> {
+    let stdin = io::stdin();
+    let mut reader = stdin.lock().lines();
+
+    loop {
+        print!("> ");
+
+        match reader.next() {
+            Some(line) => run(&line?),
+            None => break,
+        };
+    }
+
+    Ok(())
+}
 
 fn run(_source: &str) {}
